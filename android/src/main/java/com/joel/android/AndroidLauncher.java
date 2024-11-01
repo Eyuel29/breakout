1743965e819659c0f90d0bf19661da84a7190d0c
Eyuel Dejene <dejenejoel@gmail.com>
2024-11-02 00:38:59 +0300

    initial commit

---
+++ android/src/main/java/com/joel/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.joel.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.joel.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
